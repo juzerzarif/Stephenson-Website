310027d0927b0f88a07c7d4e97ece7874947a2bf
juzerzarif <juzerapj@gmail.com>
2017-12-20 17:37:13 -0600

    Add files via upload

--- stephenson.js
+++ stephenson.js
@@ -13,13 +13,16 @@ $(document).ready(function(){
     var $win = $(window);
 
     $('div.background').each(function(){
-        var scroll_speed = 10;
-        var $this = $(this);
-        $(window).scroll(function() {
-            var bgScroll = -(($win.scrollTop() - $this.offset().top)/ scroll_speed);
-            var bgPosition = 'center '+ bgScroll + 'px';
-            $this.css({ backgroundPosition: bgPosition });
-        });
+        if(screen.width > 540)
+        {
+            var scroll_speed = 10;
+            var $this = $(this);
+            $(window).scroll(function() {
+                var bgScroll = -(($win.scrollTop() - $this.offset().top)/ scroll_speed);
+                var bgPosition = 'center '+ bgScroll + 'px';
+                $this.css({ backgroundPosition: bgPosition });
+            });
+        }
     });
 
     $("#toTop").click(function ()
