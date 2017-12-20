fffe84bc1b2f30f3f522b77df9c2d45dbdbf652d
juzerzarif <juzerapj@gmail.com>
2017-12-19 22:34:54 -0600

    initial commit

---
+++ stephenson.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function(){
+    
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 600);
+    });
+
+    var $win = $(window);
+
+    $('div.background').each(function(){
+        var scroll_speed = 10;
+        var $this = $(this);
+        $(window).scroll(function() {
+            var bgScroll = -(($win.scrollTop() - $this.offset().top)/ scroll_speed);
+            var bgPosition = 'center '+ bgScroll + 'px';
+            $this.css({ backgroundPosition: bgPosition });
+        });
+    });
+
+    $("#toTop").click(function ()
+    {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+    });
+
+});
