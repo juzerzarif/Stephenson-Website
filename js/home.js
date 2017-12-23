24334f1b1fcbee4dceccdae73393c320a5bcc79a
Juzer Zarif <juzerapj@gmail.com>
2017-12-22 23:09:33 -0600

    Added form email logic and video autoplay

--- js/home.js
+++ js/home.js
@@ -1,14 +1,51 @@
 let isNavShow = false;
+let vidPause = false;
 
 function isMobile() 
 {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
+$.fn.isOnScreen = function(){
+    
+        var win = $(window);
+    
+        var viewport = {
+            top : win.scrollTop(),
+            left : win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+    
+        var bounds = this.offset();
+        bounds.right = bounds.left + this.outerWidth();
+        bounds.bottom = bounds.top + this.outerHeight();
+    
+        return (!(viewport.right < bounds.right || viewport.left > bounds.left || viewport.bottom < bounds.bottom || viewport.top > bounds.top));
+    
+    };
 
 $(document).ready(function(){
     
     alert($(window).width() + " " + $(window).height());
+    
+    document.getElementById("welcomeVideo").addEventListener("pause", function()
+    {
+        if($('video').isOnScreen()) {vidPause = true;}
+    });
+    document.getElementById("welcomeVideo").addEventListener("play", function(){ vidPause=false; });
+    $('video').each(function()
+    {
+        $this = $(this);
+        $(window).on('load scroll', function()
+        {
+            if(!vidPause)
+            {
+                if($this.isOnScreen()) { $this[0].play(); }
+                else { $this[0].pause(); }
+            }
+        });
+    });
     
     $(window).resize(function()
     {
